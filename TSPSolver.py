ea3f30b5e95175da991fe7d161966266092cd2ac
David <davidwadams12@gmail.com>
2019-04-11 12:19:20 -0600

    Added greedy solution

--- TSPSolver.py
+++ TSPSolver.py
@@ -29,12 +29,12 @@ class TSPSolver:
 		which just finds a valid random tour.  Note this could be used to find your
 		initial BSSF.
 		</summary>
-		<returns>results dictionary for GUI that contains three ints: cost of solution, 
-		time spent to find solution, number of permutations tried during search, the 
-		solution found, and three null values for fields not used for this 
-		algorithm</returns> 
+		<returns>results dictionary for GUI that contains three ints: cost of solution,
+		time spent to find solution, number of permutations tried during search, the
+		solution found, and three null values for fields not used for this
+		algorithm</returns>
 	'''
-	
+
 	def defaultRandomTour( self, time_allowance=60.0 ):
 		results = {}
 		cities = self._scenario.getCities()
@@ -65,34 +65,116 @@ class TSPSolver:
 		results['pruned'] = None
 		print(bssf.route)
 		return results
+		
+	def create_matrix(self, cities):
+		matrix = []
+		for i in range(len(cities)):
+			costs_row = []
+			for j in range(len(cities)):
+				costs_row.append(cities[i].costTo(cities[j]))
+			matrix.append(costs_row)
+		#print("Initial matrix:")
+		#print(np.copy(matrix))
+		return np.copy(matrix)
 
+	def reduce_matrix(self, matrix, initial_lower_bound, ncities):
+		lower_bound = initial_lower_bound
+		for i in range(ncities):
+			min_cost = min(matrix[i,:])
+			if min_cost == 0:
+				continue
+			elif min_cost < math.inf:
+					matrix[i,:] -= min_cost
+					lower_bound += min_cost
+		for j in range(ncities):
+			min_cost = min(matrix[:,j])
+			if min_cost == 0:
+				continue
+			elif min_cost < math.inf:
+					matrix[:,j] -= min_cost
+					lower_bound += min_cost
+		#print("Reduced:")
+		#print(np.copy(matrix))
+		#print("Lower bound:",lower_bound)
+		return matrix, lower_bound
 
 	''' <summary>
-		This is the entry point for the greedy solver, which you must implement for 
+		This is the entry point for the greedy solver, which you must implement for
 		the group project (but it is probably a good idea to just do it for the branch-and
 		bound project as a way to get your feet wet).  Note this could be used to find your
 		initial BSSF.
 		</summary>
-		<returns>results dictionary for GUI that contains three ints: cost of best solution, 
+		<returns>results dictionary for GUI that contains three ints: cost of best solution,
 		time spent to find best solution, total number of solutions found, the best
-		solution found, and three null values for fields not used for this 
-		algorithm</returns> 
+		solution found, and three null values for fields not used for this
+		algorithm</returns>
 	'''
 
 	def greedy( self,time_allowance=60.0 ):
-		pass
-	
-	
-	
+		results = {}
+		cities = self._scenario.getCities()
+		ncities = len(cities)
+		reduced_matrix, lower_bound = self.reduce_matrix(self.create_matrix(cities), 0, ncities)
+		foundTour = False
+		count = 0
+		bssf = None
+		start_time = time.time()
+		row = 0
+		path = [row]
+		parent_matrix = np.copy(reduced_matrix)
+		states = [(parent_matrix,lower_bound,0,path)] #matrix, lower_bound, which node you're coming from
+		while not foundTour and time.time()-start_time < time_allowance:
+			current_state = states.pop() #should get next minimum value
+			current_matrix = np.copy(current_state[0])
+			current_lower_bound = current_state[1]
+			current_depth = current_state[2]
+
+			current_row = current_state[3][-1]
+			current_path = current_state[3]
+			if current_depth == ncities:
+				if current_row == 0:
+					#found solution
+					foundTour = True
+					count += 1
+					bssf = TSPSolution([cities[x] for x in current_path[:-1]])
+				else:
+					#invalid solution
+					continue
+			else:
+				current_matrix, current_lower_bound = self.reduce_matrix(current_matrix, current_lower_bound, ncities)
+				children_costs = np.copy(current_matrix[current_row])
+				children_costs = [(children_costs[i],i) for i in range(len(children_costs))]
+				descending_costs = sorted(children_costs, reverse=True) #makes minimum cost get appended last
+				for cost in descending_costs:
+					child_matrix = np.copy(current_matrix)
+					if cost[0] < math.inf:
+						column = cost[1]
+						child_lower_bound = current_lower_bound + cost[0]
+						child_matrix[current_row,:] = math.inf
+						child_matrix[:,column] = math.inf
+						child_matrix[column,current_row] = math.inf
+						states.append((child_matrix,child_lower_bound,current_depth + 1, current_path + [column]))
+		end_time = time.time()
+		results['cost'] = bssf.cost if foundTour else math.inf
+		results['time'] = end_time - start_time
+		results['count'] = count
+		results['soln'] = bssf
+		results['max'] = None
+		results['total'] = None
+		results['pruned'] = None
+		return results
+
+
+
 	''' <summary>
 		This is the entry point for the branch-and-bound algorithm that you will implement
 		</summary>
-		<returns>results dictionary for GUI that contains three ints: cost of best solution, 
+		<returns>results dictionary for GUI that contains three ints: cost of best solution,
 		time spent to find best solution, total number solutions found during search (does
-		not include the initial BSSF), the best solution found, and three more ints: 
-		max queue size, total number of states created, and number of pruned states.</returns> 
+		not include the initial BSSF), the best solution found, and three more ints:
+		max queue size, total number of states created, and number of pruned states.</returns>
 	'''
-		
+
 	def branchAndBound( self, time_allowance=60.0 ):
 		pass
 		# cities = self._scenario.getCities()
@@ -121,15 +203,11 @@ class TSPSolver:
 	''' <summary>
 		This is the entry point for the algorithm you'll write for your group project.
 		</summary>
-		<returns>results dictionary for GUI that contains three ints: cost of best solution, 
-		time spent to find best solution, total number of solutions found during search, the 
+		<returns>results dictionary for GUI that contains three ints: cost of best solution,
+		time spent to find best solution, total number of solutions found during search, the
 		best solution found.  You may use the other three field however you like.
-		algorithm</returns> 
+		algorithm</returns>
 	'''
-		
+
 	def fancy( self,time_allowance=60.0 ):
 		pass
-		
-
-
-
